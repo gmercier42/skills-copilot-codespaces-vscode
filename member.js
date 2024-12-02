dc451a49f2264c469a15823802cb836b04f6ed13
Guillaume Mercier <hello@guillaumemercier.com>
2024-12-02 11:53:56 -0500

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: 'John',
+        age: 30,
+        skills: ['JavaScript', 'HTML', 'CSS'],
+        isMember: true
+    };
+    return member;
+}
